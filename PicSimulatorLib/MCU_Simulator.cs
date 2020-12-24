1c1b9ab1abfe28047a11a144a5ab48cbc354df21
EsseivaN <nicolas.esseiva@gmail.com>
2020-12-24 17:38:37 +0100

    Update MCU_Simulator.cs

--- PicSimulatorLib/MCU_Simulator.cs
+++ PicSimulatorLib/MCU_Simulator.cs
@@ -14,6 +14,25 @@ namespace PicSimulatorLib
 
         private int registerPerBank = -1, bankCount = -1;
 
+        // Pour les bank et addresses :
+        /* Indiquer combien de bits utilisés (12 pour 1827)
+         * Indiquer combien de bits pour le bank
+         * Indiquer combien de bits pour l'adresse
+         * Indiquer le nombre de banks
+         * Indiquer les bytes par bank
+         * 
+         * Donc :
+         * Indiquer nombre total data memory
+         * Indiquer le nombre de bank 
+         * 
+         * On fait :
+         * total / banks = 
+         * 
+         * 
+         * 
+         * 
+         */
+
         private void PopulateRegisters(int bankCount, int registerPerBank)
         {
             this.bankCount = bankCount;
